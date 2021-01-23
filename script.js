8f8debbbdc50c544ca3ed71c4b55f727a7d635b3
rroyalty <rroyalty@gmail.com>
2021-01-23 13:17:05 -0500

    Getting a scroll feature working!

--- script.js
+++ script.js
@@ -4,21 +4,38 @@ $(document).ready(function() {
     let scheduleStart = $("#schedule");
     let displayClock = init();
 
-    let playerArray = [].slice.call ($(".playerList"));
+    let rowArray = $(".row");
+    
+    let timeHour = dt.local().set({hour: dt.local().hour, minute: "0", second: "0"});
+
+    for(let i = 0; i < rowArray.length; i++ ) {
+        formatRow(i, 0);
+    }
+
+    console.log(rowArray);
 
     $("#schedule").on('wheel', function(e) {
-
         clearInterval(displayClock);
         var delta = e.originalEvent.deltaY;
       
-        if (delta < 0) {}// going down
-        else {}; // going up
+        if (delta < 0) {
+            $(rowArray[rowArray.length - 1]).remove();
+            $(scheduleStart).prepend(rowTemplate);
+            rowArray = $(".row");
+            formatRow(0);
+        }// going down
+        else {
+            $(rowArray[0]).remove();
+            $(scheduleStart).append(rowTemplate);
+            rowArray = $(".row");
+            formatRow(rowArray.length - 1);
+        }; // going up
 
         return false;
       });
 
     function init() {
-        let dtInit = dt.local().toLocaleString(dt.DATETIME_MED_WITH_SECONDS);
+        let dtInit = dt.local().weekdayLong + ", " + dt.local().toLocaleString(dt.DATETIME_MED_WITH_SECONDS);
         let dtDisplay = $("#currentDay");
         let screenHeight = screen.availHeight;
         let rowCount = Math.floor((screenHeight - 166.7)/80)
@@ -31,10 +48,18 @@ $(document).ready(function() {
 
         let displayClock = setInterval (function() {
             dtDisplay.text(dtInit);
-            dtInit = dt.local().plus({seconds: 1}).toLocaleString(dt.DATETIME_MED_WITH_SECONDS);
+            dtInit = dt.local().weekdayLong  + ", " + dt.local().plus({seconds: 1}).toLocaleString(dt.DATETIME_MED_WITH_SECONDS);
         }, 1000);
 
         return displayClock;
+    }
+
+    function formatRow(i, j) {
+        $(rowArray[i]).text(dt.local().toLocaleString(dt.DATE_SHORT) +"\n" + timeHour.plus({hours: i}).toLocaleString(dt.TIME_SIMPLE));
+
+        if (timeHour.hour + i === dt.local().hour) $(rowArray[i]).addClass("present");
+        else if (timeHour.hour + i > dt.local().hour) $(rowArray[i]).addClass("future");
+        else $(rowArray[i]).addClass("past");
     }
 
 });
