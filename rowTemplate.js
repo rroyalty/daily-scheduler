ed2582661ecfd0dead220fcbe3a167c3f14a943e
rroyalty <rroyalty@gmail.com>
2021-01-22 20:19:34 -0500

    tinkering

--- rowTemplate.js
+++ rowTemplate.js
@@ -1,1 +1,22 @@
-const rowTemplate = '<div><div class="row locked"><div class="col-2"><p>Test Col1</p></div><div class="col-10"><p>Test Col2</p></div></div><div class="row input" hidden="true"><form><div class="col-2"><p>Test Col1</p></div><div class="col-8"><p>Test Col2</p></div><div class="col-2"><p>Test Col3</p></div></form></div></div>'
+const rowTemplate = 
+
+{/* <div>
+    <div class="row locked">
+        <div class="col-2">
+            <p>Test Col1</p>
+        </div>
+        <div class="col-10">
+            <p>Test Col2</p>
+        </div>
+    </div>
+    <div class="row entry">
+        <form>
+            <button type="button" class="col-2">
+                <p>Test Col1</p>
+            </button>
+            <div class="col-10">
+                <p>Test Col2</p>
+            </div>
+        </form>
+    </div>
+</div> */}
