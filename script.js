30fbe21af3192a14f654f68e57e70bd3f8a74dd1
rroyalty <rroyalty@gmail.com>
2021-01-24 10:19:38 -0500

    Things coming along nicely. Working on buttons.

--- script.js
+++ script.js
@@ -1,37 +1,65 @@
 $(document).ready(function() {
-
-    const rowTemp = '<div class="row"></row>' 
-    const timeKeyTemp = '<div class="col-2 col-sm-1 locked rpmx"></div>'
-    const timeKeyPara = '<p class="timeKey"></p>'
-    const saveButton = '<div class="col-2 col-sm-1 btn btn-primary unlocked rpmx" hidden="true"></div>'
-    const entryField = '<textarea class="col-10 col-sm-11 unlocked entry rpmx" rows="3" disabled></textarea>'
-
-
-const schedEntries = { entries: [{}]}
-
-
+ 
+    const schedEntries = { entries: [{}]};
     const dt = luxon.DateTime;
+    const screenHeight = screen.availHeight;
+    const rowCount = Math.floor((screenHeight - 166.7)/80)
+    let lockToggle = false;
     let position = 0;
     let scheduleStart = $("#schedule");
-    let displayClock = init();
 
-    let rowArray = $(".row");
+    let rowArray = init();
     let entryArray = $(".entry");
-    let buttonArray = $(".btn");
     let timeArray = $(".timeKey");
-    
-    
+        
     let timeHour = dt.local().set({hour: dt.local().hour, minute: "0", second: "0"});
 
-    for(let i = 0; i < rowArray.length; i++ ) {
+    for(let i = 0; i < rowArray.length; i++) {
         formatRow(i, 0);
     }
 
     console.log(rowArray);
 
-    $(entryArray).on('click', function() {
+    $(rowArray).on('click', function() {
+
+        if (lockToggle === true) return false;
+        
+        lockToggle = true;
         let _this = this;
-        console.log(_this);
+
+        let rowID = $(_this).attr("id");
+
+        lockAllEntries();
+
+        let saveButton = $(_this).children(".unlocked").eq(0);
+        let timeKey = $(_this).children(".locked").eq(0);
+        let textArea = $(_this).children(".entry").eq(0);
+        $(textArea).prop("disabled", false);
+        $(saveButton).prop("hidden", false);
+        $(timeKey).prop("hidden", true);
+        textArea.focus();
+
+    });
+
+    $(rowArray).on('click', ".unlocked", function() {
+
+        if (lockToggle === true) return false;
+        
+        lockToggle = true;
+        let _this = this;
+
+        let rowID = $(_this).attr("id");
+
+        lockAllEntries();
+
+        let saveButton = $(_this).children(".unlocked").eq(0);
+        let timeKey = $(_this).children(".locked").eq(0);
+        let textArea = $(_this).children(".entry").eq(0);
+        $(textArea).prop("disabled", false);
+        $(saveButton).prop("hidden", false);
+        $(timeKey).prop("hidden", true);
+        textArea.focus();
+
     });
 
     $("#schedule").on('wheel', function(e) {
@@ -51,12 +79,14 @@ const schedEntries = { entries: [{}]}
     function init() {
         let dtInit = dt.local().weekdayLong + ", " + dt.local().toLocaleString(dt.DATETIME_MED_WITH_SECONDS);
         let dtDisplay = $("#currentDay");
-        let screenHeight = screen.availHeight;
-        let rowCount = Math.floor((screenHeight - 166.7)/80)
 
-        for(let i = 1; i < rowCount; i++) {
-            appendRow();
+        for(let i = 0; i < rowCount - 1; i++) {
+            appendRow(i);
         }
+
+        let rowArray = $(".row");
+
+        rowChildren(rowArray);
 
         dtDisplay.text(dtInit);
 
@@ -65,40 +95,70 @@ const schedEntries = { entries: [{}]}
             dtInit = dt.local().weekdayLong  + ", " + dt.local().plus({seconds: 1}).toLocaleString(dt.DATETIME_MED_WITH_SECONDS);
         }, 1000);
 
-        return displayClock;
+        return rowArray;
     }
 
     function formatRow(i, j) {
 
+        rowArray = $(".row");
+        entryArray = $(".entry");
+        timeArray = $(".timeKey");
+        
         $(timeArray[i]).text(timeHour.plus({hours: i + j}).toLocaleString(dt.DATE_SHORT) +"\n" + timeHour.plus({hours: i + j}).toLocaleString(dt.TIME_SIMPLE));
 
-        if (timeHour.hour + i + j === dt.local().hour) $(timeArray[i]).addClass("present");
-        else if (timeHour.hour + i + j > dt.local().hour) $(timeArray[i]).addClass("future");
-        else $(timeAr[i]).addClass("past");
+        if (timeHour.hour + i + j === dt.local().hour) $(rowArray[i]).addClass("present");
+        else if (timeHour.hour + i + j > dt.local().hour) $(rowArray[i]).addClass("future");
+        else $(rowArray[i]).addClass("past");
     }
 
     function scrollDown() {
+        if (lockToggle === true) return false;
+
         position--; 
         $(rowArray[rowArray.length - 1]).remove();
-        $(scheduleStart).prepend(rowTemplate);
+        prependRow(position);
         rowArray = $(".row");
+        rowChildren(rowArray[0]);
         formatRow(0, position);
     }
 
     function scrollUp() {
+        if (lockToggle === true) return false;
+
         position++; 
         $(rowArray[0]).remove();
-        $(scheduleStart).append(rowTemp);
+        appendRow(position + rowArray.length - 1);
         rowArray = $(".row");
+        rowChildren(rowArray[rowArray.length - 1]);
         formatRow(rowArray.length - 1, position);
     }
 
-    function appendRow() {
-        let rowGen = $(scheduleStart).append(rowTemp);
-        console.log(rowGen);
-        rowGen.append(timeKeyTemp).append(timeKeyPara);
-        rowGen.append(saveButton);
-        rowGen.append(entryField);
+    function appendRow(index) {
+        let rowTemp = '<div id="#entry_' + index + '" class="row"></div>' 
+        $(scheduleStart).append(rowTemp);
+    }
+
+    function prependRow(index) {
+        let rowTemp = '<div id="#entry_' + index + '" class="row"></div>' 
+        $(scheduleStart).prepend(rowTemp);
+    }
+
+
+    function rowChildren(rowsToAppend) {
+        let timeKeyTemp = '<div class="col-2 col-sm-1 locked rpmx"></div>'
+        let saveButton = '<div class="col-2 col-sm-1 btn btn-primary unlocked rpmx" hidden="true"></div>'
+        let entryField = '<textarea class="col-10 col-sm-11 entry rpmx" rows="3" disabled></textarea>'
+        let timeKeyPara = '<p class="timeKey"></p>'
+        $(timeKeyTemp).appendTo(rowsToAppend).append(timeKeyPara);
+        $(saveButton).appendTo(rowsToAppend);
+        $(entryField).appendTo(rowsToAppend);
+    };
+
+    function lockAllEntries() {
+        $(".locked").prop("hidden", false);
+        $(".unlocked").prop("hidden", true);
+        $(".entry").prop("disabled", true);
     }
 
 });
+
