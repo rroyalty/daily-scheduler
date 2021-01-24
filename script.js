0c76851ed6b2f790ba73f94d1b2baba0ec444348
rroyalty <rroyalty@gmail.com>
2021-01-24 17:13:28 -0500

    Comments

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 $(document).ready(function() {
  
+    // Initial Variables
     const dt = luxon.DateTime;
-    const screenHeight = screen.availHeight;
-    const rowCount = Math.floor((screenHeight - 166.7)/80)
     let lockToggle = false;
     let position = 0;
     let scheduleStart = $("#schedule");
 
+    // Available screen height.
+    const screenHeight = screen.availHeight;
+
+    // Only few enough rows that the current screen can display are created based off of element sizes and available screen height.
+    const rowCount = Math.floor((screenHeight - 166.7)/80)
+
+    // Initialize Row Collection and Start Clock.
     let rowArray = init();
+
+    // Other Collections.
     let entryArray = $(".entry");
     let timeArray = $(".timeKey");
     let buttonArray = $(".unlocked");
-        
+    
+    // Current Time to the Hour.
     let timeHour = dt.local().set({hour: dt.local().hour, minute: "0", second: "0"});
 
+    // Format freshley created rows elements.
     for(let i = 0; i < rowArray.length; i++) {
         formatRow(i, 0);
     }
 
+    // Set the Event Handlers for the initial set of rows.
     setEventHandler(rowArray);
 
+    // Available keyboard events (Mousewheel and Arrow Keys work counterintuitively. Hence, why Down Arrow calls Scroll Up... etc.):
     $(document).on("keydown", function(event) {
-        console.log(event.keyCode);
+
         switch(true) {
+            // Down Arrow: Scroll Down
             case event.keyCode === 40:
                 scrollUp();
             break;
+
+            // Up Arrow: Scroll Up
             case event.keyCode === 38:
                 scrollDown();
             break;
+
+            // Home Button: Return to Current Time
             case event.keyCode === 36:
                 $(".row").remove();
                 rowArray = init();
@@ -38,9 +55,13 @@ $(document).ready(function() {
                 }
                 setEventHandler(rowArray);
             break;
+
+            // Page Up button: Scroll Up by a full page.
             case event.keyCode === 33:
                 for(i = 0; i < rowCount - 1; i++) scrollDown();
             break;
+
+            // Page Down button: Scroll Down by a full page.
             case event.keyCode === 34:
                 for(i = 0; i < rowCount - 1; i++) scrollUp();
             break;
@@ -48,43 +69,42 @@ $(document).ready(function() {
 
     });
 
-    $("#schedule").on('wheel', function(e) {
-        // clearInterval(displayClock);
-        var delta = e.originalEvent.deltaY;
+    // Mouse Wheel Events. Scroll through time entries. (Code taken from a solution on Stack Overflow.)
+    $("#schedule").on('wheel', function(event) {
+
+        let delta = event.originalEvent.deltaY;
       
+        // going down
         if (delta < 0) {
             scrollDown();
-        }// going down
+        }
+        // going up
         else {
             scrollUp();
-        };// going up
+        };
 
         return false;
       });
 
-    $("#schedule").on('swipe', function(event) {
 
-        if (event.deltaY < 0) {
-            scrollUp();
-        } else {
-            scrollDown();
-            }
-    });
-
+    // Initialize Function: Start clock and create initial set of rows.
     function init() {
         let dtInit = dt.local().weekdayLong + ", " + dt.local().toLocaleString(dt.DATETIME_MED_WITH_SECONDS);
         let dtDisplay = $("#currentDay");
 
+        // Create rows.
         for(let i = 0; i < rowCount - 1; i++) {
             appendRow(i);
         }
 
         let rowArray = $(".row");
 
+        // Create Row children objects.
         rowChildren(rowArray);
 
         dtDisplay.text(dtInit);
 
+        // Create clock.
         let displayClock = setInterval (function() {
             dtDisplay.text(dtInit);
             dtInit = dt.local().weekdayLong  + ", " + dt.local().plus({seconds: 1}).toLocaleString(dt.DATETIME_MED_WITH_SECONDS);
@@ -93,6 +113,7 @@ $(document).ready(function() {
         return rowArray;
     }
 
+    // Format the rows: Past/present/future. Load local data into rows as they are created.
     function formatRow(i, j) {
 
         rowArray = $(".row");
@@ -109,6 +130,7 @@ $(document).ready(function() {
         else $(rowArray[i]).addClass("past");
     }
 
+    // Scroll Down towards future events.
     function scrollDown() {
         if (lockToggle) return false;
         position--; 
@@ -120,6 +142,7 @@ $(document).ready(function() {
         setEventHandler(rowArray);
     }
 
+    // Scroll up towards past events.
     function scrollUp() {
         if (lockToggle) return false;
         position++; 
@@ -131,16 +154,19 @@ $(document).ready(function() {
         setEventHandler(rowArray);
     }
 
+    // Append a 'future' row.
     function appendRow(index) {
         let rowTemp = '<div id="#entry_' + index + '" class="row"></div>' 
         $(scheduleStart).append(rowTemp);
     }
 
+    // Prepend a 'past' row.
     function prependRow(index) {
         let rowTemp = '<div id="#entry_' + index + '" class="row"></div>' 
         $(scheduleStart).prepend(rowTemp);
     }
 
+    // Append all child objects necessary to rows.
     function rowChildren(rowsToAppend) {
         let timeKeyTemp = '<div class="d-inline-flex justify-content-center col-4 col-md-3 col-lg-1 locked rpmx"></div>'
         let saveButton = '<div class="d-inline-flex d-none col-4 col-md-3 col-lg-1 btn btn-primary align-items-center justify-content-center unlocked rpmx">Save Entry</div>'
@@ -151,12 +177,14 @@ $(document).ready(function() {
         $(entryField).appendTo(rowsToAppend);
     };
 
+    // Locks all entries to editting.
     function lockAllEntries() {
         $(".locked").removeClass("d-none");
         $(".unlocked").addClass("d-none");
         $(".entry").prop("disabled", true);
     }
 
+    // Sets event handlers for rows as they are created.
     function setEventHandler(rows) {
         $(rows).on('click', function(event) {
             let _this = this;
